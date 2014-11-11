bae3d74d86eecfa876b5fea8cf2bc04d45275a5e
cyurtbil <cenky92@gmail.com>
2014-11-10 19:21:18 -0500

    now client-sdie script can pass the name of the customer to rails server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,26 @@
 
 Stripe.setPublishableKey('pk_test_0fbtu0To5Q8TurGcFy6XZ505');
 
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+
+  if (response.error) {
+    // Show the errors on the form
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    // response contains id and card, which contains additional card details
+    var token = response.id;
+    var fullName = response.card.name;
+    debugger;
+    // Insert the token into the form so it gets submitted to the server
+    $form.append($('<input type="hidden" name="user[access_token]" />').val(token));
+    $form.append($('<input type="hidden" name="user[name]" />').val(fullName));
+    // and submit
+    $form.get(0).submit();
+  }
+}
+
 $(document).ready(function() {
   $('#payment-form').submit(function(event) {
     var $form = $(this);
@@ -15,20 +35,3 @@ $(document).ready(function() {
     return false;
   });
 });
-
-function stripeResponseHandler(status, response) {
-  var $form = $('#payment-form');
-
-  if (response.error) {
-    // Show the errors on the form
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('button').prop('disabled', false);
-  } else {
-    // response contains id and card, which contains additional card details
-    var token = response.id;
-    // Insert the token into the form so it gets submitted to the server
-    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-    // and submit
-    $form.get(0).submit();
-  }
-};
