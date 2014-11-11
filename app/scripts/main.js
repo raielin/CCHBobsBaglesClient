f84ddb24132748db237fdb761a661633d52988a2
cyurtbil <cenky92@gmail.com>
2014-11-11 10:30:41 -0500

    set up a navbar with bootstrap and defined my routes via backbone router

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,49 @@
 'use strict';
 
-Stripe.setPublishableKey('pk_test_0fbtu0To5Q8TurGcFy6XZ505');
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'home',
+    'home': 'home',
+    'menu': 'menu',
+    'about': 'about',
+    'contact': 'contact',
+    'checkout': 'checkout'
+  },
 
-function stripeResponseHandler(status, response) {
-  var $form = $('#payment-form');
-
-  if (response.error) {
-    // Show the errors on the form
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('button').prop('disabled', false);
-  } else {
-    // response contains id and card, which contains additional card details
-    var token = response.id;
-    var fullName = response.card.name;
-    // Insert the token into the form so it gets submitted to the server
-    $form.append($('<input type="hidden" name="user[access_token]" />').val(token));
-    $form.append($('<input type="hidden" name="user[name]" />').val(fullName));
-    // and submit
-    $form.get(0).submit();
-  }
-}
-
-$(document).ready(function() {
-  $('#payment-form').submit(function(event) {
-    var $form = $(this);
-
-    // Disable the submit button to prevent repeated clicks
-    $form.find('button').prop('disabled', true);
-
-    Stripe.card.createToken($form, stripeResponseHandler);
-
-    // Prevent the form from submitting with the default action
-    return false;
-  });
+  
 });
+
+// Stripe.setPublishableKey('pk_test_0fbtu0To5Q8TurGcFy6XZ505');
+
+// function stripeResponseHandler(status, response) {
+//   var $form = $('#payment-form');
+
+//   if (response.error) {
+//     // Show the errors on the form
+//     $form.find('.payment-errors').text(response.error.message);
+//     $form.find('button').prop('disabled', false);
+//   } else {
+//     // response contains id and card, which contains additional card details
+//     var token = response.id;
+//     var fullName = response.card.name;
+//     // Insert the token into the form so it gets submitted to the server
+//     $form.append($('<input type="hidden" name="user[access_token]" />').val(token));
+//     $form.append($('<input type="hidden" name="user[name]" />').val(fullName));
+//     // and submit
+//     $form.get(0).submit();
+//   }
+// }
+
+// $(document).ready(function() {
+//   $('#payment-form').submit(function(event) {
+//     var $form = $(this);
+
+//     // Disable the submit button to prevent repeated clicks
+//     $form.find('button').prop('disabled', true);
+
+//     Stripe.card.createToken($form, stripeResponseHandler);
+
+//     // Prevent the form from submitting with the default action
+//     return false;
+//   });
+// });
