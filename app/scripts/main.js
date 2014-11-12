2fe79b088f73030c9a4a1a207f44cc3f497b93d4
cyurtbil <cenky92@gmail.com>
2014-11-11 23:29:41 -0500

    created about page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,13 @@
 
 // Handlebars.registerPartial('cart', Handlebars.templates.cart);
 
+$(document).ready(function() {
+   $('#list li').on('click', function() {
+      var name = $(this).val() 
+      document.cookie = "name=" + name;
+    });
+});
+
 var Router = Backbone.Router.extend({
   routes: {
     '': 'home',
@@ -20,6 +27,7 @@ var Router = Backbone.Router.extend({
 
   menu: function() {
     var template = Handlebars.compile($("#menu-temp").html());
+
     $.ajax({
       url: 'http://localhost:3000/menus',
       type: 'GET'
